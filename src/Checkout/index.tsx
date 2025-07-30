c4750c9a5bf9f61771615f2559fd76aa8479f064
Cleiton Barros <cleiton.junior@aluno.faculdadeimpacta.com.br>
2025-07-30 15:43:11 -0300

    Ajuste na página Checkout

--- src/Checkout/index.tsx
+++ src/Checkout/index.tsx
@@ -1,59 +1,72 @@
-import { Address, Complete, FirstField, 
-  Input, Input1, Input2, InputsAddress, SecondField, 
-  TitleM, TitleS } from "./styles";
-import { MapPin, CurrencyDollar } from 'phosphor-react';
+import { Address, ChoosePayment, CoffeeSection, CoffeeTitle, Complete,  
+  CompletePayment,  
+  Dollar, FirstField, Input, 
+  Input1, Input2, InputsAddress, 
+  Payment, ReviewOrder, SecondField, SectionButtons, 
+  SectionCheck, TitleM, TitleS } from "./styles";
+import { MapPin, CurrencyDollar, CreditCard, Money, Bank } from 'phosphor-react';
+
 
 export function Checkout() {
   return (
-    <div>
-      <Complete>Complete seu pedido</Complete>
-      <FirstField>
+    <SectionCheck>
+      <CompletePayment>
+        <Complete>Complete seu pedido</Complete>
+        <FirstField>
 
-        <Address>
-          <TitleM>
-            <MapPin color='#C47F17' size={15} />
-            <span>Endereço de entrega</span>
-          </TitleM>
-          <TitleS>Informe o endereço onde deseja receber seu pedido</TitleS>
-        </Address>
+          <Address>
+            <TitleM>
+              <MapPin color='#C47F17' size={15} />
+              <span>Endereço de entrega</span>
+            </TitleM>
+            <TitleS>Informe o endereço onde deseja receber seu pedido</TitleS>
+          </Address>
 
-        <InputsAddress>
-            <Input type="text" placeholder="CEP" width="30%"/>
-            <Input type="text" placeholder="Rua"/>
+          <InputsAddress>
+              <Input type="text" placeholder="CEP" width="30%"/>
+              <Input type="text" placeholder="Rua"/>
 
-          <Input1>
-            <Input type="number" placeholder="Número" width="30%" />
-            <Input type="text" placeholder="Complemento" width="70%" />
-          </Input1>
+            <Input1>
+              <Input type="number" placeholder="Número" width="30%" />
+              <Input type="text" placeholder="Complemento" width="70%" />
+            </Input1>
 
-          <Input2>
-            <Input type="text" placeholder="Bairro" width="30%" />
-            <Input type="text" placeholder="Cidade" width="60%" />
-            <Input type="text" placeholder="UF" width="10%" />
-          </Input2>
-        </InputsAddress>
+            <Input2>
+              <Input type="text" placeholder="Bairro" width="30%" />
+              <Input type="text" placeholder="Cidade" width="60%" />
+              <Input type="text" placeholder="UF" width="10%" />
+            </Input2>
+          </InputsAddress>
 
-      </FirstField>
+        </FirstField>
 
-      <SecondField>
-        <TitleM>
-          <CurrencyDollar color='#8047F8' size={15} />
-          <span>Pagamento</span>
-          <TitleS>O pagamento é feito na entrega. Escolha a forma que deseja pagar</TitleS>
-        </TitleM>
-        
-        <div>
-          <button type="button">Cartão de crédito</button>
-          <button type="button">Cartão de débito</button>
-          <button type="button">Dinheiro</button>
-        </div>
-      </SecondField>
+        <SecondField>
+          <Payment>
+            <Dollar>
+              <CurrencyDollar color='#8047F8' size={20} />
+              <span>Pagamento.</span>
+            </Dollar>
+            <ChoosePayment>
+              O pagamento é feito na entrega. Escolha a forma que deseja pagar
+            </ChoosePayment>
+          </Payment>
+          
+          <SectionButtons>
+            <button><CreditCard color='#8047F8' size={20} />Cartão de crédito</button>
+            <button><Bank color='#8047F8' size={20} />Cartão de débito</button>
+            <button><Money color='#8047F8' size={20} />Dinheiro</button>
+          </SectionButtons>
+        </SecondField>
+      </CompletePayment>
 
-        <div>
-          <TitleM>Cafés selecionados</TitleM>
+      <CoffeeSection>
+        <CoffeeTitle>Cafés selecionados</CoffeeTitle>
+        <ReviewOrder>
           <span>Revise seus itens de pedido</span>
           {/* Aqui você pode mapear os cafés selecionados */}
-        </div>
-    </div>
+        </ReviewOrder>
+      </CoffeeSection>
+
+    </SectionCheck>
   )
 }
--- src/Checkout/styles.ts
+++ src/Checkout/styles.ts
@@ -4,12 +4,30 @@ interface InputProps {
     width?: string;
 }
 
+export const SectionCheck = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+    align-items: flex-start;
+    gap: 2rem;
+    width: 100%;
+    padding: 2rem;
+`;
+
+export const CompletePayment = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 60%;
+
+
+`;
+
 export const Complete = styled.h1`
     font-size: 1.125rem;
     display: flex;
     align-items: center;
     justify-content: space-between;
-    margin-left: 10rem;
+
 `;
 export const TitleM = styled.div`
     font-size: 1rem;
@@ -37,11 +55,10 @@ export const FirstField = styled.div`
     margin-bottom: 2rem;
     background-color: ${(props) => props.theme.colors["base-card"]};
     border-radius: 6px;
+    width: 100%;
 
     display: flex;
     flex-direction: column;
-    width:40%;
-    margin-left: 10rem;
     padding: 2.5rem;
     gap: 2rem;
 `;
@@ -80,8 +97,7 @@ export const SecondField = styled.div`
 
     display: flex;
     flex-direction: column;
-    width:40%;
-    margin-left: 10rem;
+    width: 100%;
     padding: 2.5rem;
     gap: 2rem;
 
@@ -98,4 +114,89 @@ export const SecondField = styled.div`
             background-color: ${(props) => props.theme.colors["base-hover"]};
         }
     }
+`;
+
+export const Payment = styled.div`
+
+    display: flex;
+    flex-direction: column;
+    gap:0.5rem;
+
+    font-size: 0.875rem;
+    font-family: ${(props) => props.theme.fonts.regular};
+    color: ${(props) => props.theme.colors["base-text"]};
+`;
+
+export const ChoosePayment = styled.span`
+    font-size: 0.75rem;
+    color: ${(props) => props.theme.colors["base-text"]};
+    font-family: ${(props) => props.theme.fonts.regular};
+    margin-left: 1.7rem;
+`;
+
+export const Dollar = styled.div`
+display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  font-size: 1rem;
+  color: ${(props) => props.theme.colors["base-text"]};
+  font-weight: 500;
+
+  span {
+    color: ${(props) => props.theme.colors["base-text"]};
+    font-size: 1rem;
+    font-family: ${(props) => props.theme.fonts.regular};
+  }
+
+`;
+
+export const SectionButtons = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 0.75rem;
+    justify-content: space-between;
+    width: 100%;
+
+    button {
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+        padding: 0.75rem;
+        justify-content: center;
+        border: 1px solid ${(props) => props.theme.colors["base-button"]};
+        border-radius: 6px;
+        background-color: ${(props) => props.theme.colors["base-button"]};
+        cursor: pointer;
+        color: ${(props) => props.theme.colors["base-text"]};
+        flex: 1;
+    }
+`;
+
+export const CoffeeSection = styled.div`
+    background-color: ${(props) => props.theme.colors["base-card"]};
+    border-radius: 6px;
+    padding: 2rem;
+    width: 38%;
+
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+`;
+
+export const CoffeeTitle = styled.h2`
+    font-size: 1.25rem;
+    font-family: ${(props) => props.theme.fonts.title};
+    color: ${(props) => props.theme.colors["base-subtitle"]};
+`;
+
+export const ReviewOrder = styled.div`
+    background-color: ${(props) => props.theme.colors["base-card"]};
+    border-radius: 6px;
+    padding: 2rem;
+    width: 100%;
+
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    
 `;
